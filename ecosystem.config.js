6a607dd33b295fbc5b3351f9edf81dcee76380dc
Can Yilmaz <spawnedc@gmail.com>
2018-01-07 15:06:08 +0000

    Setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'odengec-server',
+    script: './dist/server.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-52-215-85-246.eu-west-1.compute.amazonaws.com',
+      key: '~/.ssh/odengec-server.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:spawnedc/odengec-server.git',
+      path: '/home/ubuntu/odengec-server',
+      'post-deploy': 'npm install && npm build && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
